b8d720bed54051f3f6ff60d6151eba862ca0d9ff
ArturoCeron <arturo.ceron17@tectijuana.edu.mx>
2021-11-08 21:55:08 -0800

    fix plot sizes

--- Unit_2/Evaluation/Evaluation.R
+++ Unit_2/Evaluation/Evaluation.R
@@ -20,7 +20,7 @@ Data1<-Data1[which(Data1$Studio=="Buena Vista Studios"|Data1$Studio=="Fox"|
 #Graficar utilizando 2 facetas, jitter con los parametros de budget y color
 #y boxplot haciendolo transparente y sin valores fuera de la caja
 p <- ggplot(Data1, aes(x=Genre, y=Gross...US))+
-  geom_jitter(aes(size=Budget...mill.,color=Studio))+
+  geom_jitter(shape = 20, aes(size = Budget...mill.,color=Studio))+
   geom_boxplot(size=0.5, alpha=0.7, outlier.shape = NA)                  
 #Agregar diseño al texto de la grafica
 p +
