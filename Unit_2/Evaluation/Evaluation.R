002d6d3b64366eba52b5bcc9acf58efa7283ade0
Your Name <ariel.landa17@tectijuana.edu.mx>
2021-11-08 18:19:08 -0800

    Filtrar datos y mostrar jitter

--- Unit_2/Evaluation/Evaluation.R
+++ Unit_2/Evaluation/Evaluation.R
@@ -3,6 +3,22 @@ Data <- read.csv("Project-Data.csv")
 
 #Libreria, importar si no se tiene
 library(ggplot2)
-#Limitamos a los generos propuestos
-lim<-c("action","adventure","animation","comedy","drama")
-Datalim<-Data[which(Data$Genre==lim)]
+library(dplyr)
+head(Data)
+colnames(Data) 
+head(Data)
+tail(Data)
+str(Data)
+summary(Data)
+
+#Limitar por genero
+Data1<-Data[which(Data$Genre=="action"|Data$Genre=="adventure"|
+                    Data$Genre=="animation"|Data$Genre=="comedy"|
+                    Data$Genre=="drama"),]
+#Limitar por estudio
+Data1<-Data1[which(Data1$Studio=="Buena Vista Studios"|Data1$Studio=="Fox"|
+                     Data1$Studio=="Paramount Pictures"|Data1$Studio=="Sony"|
+                     Data1$Studio=="Universal"|Data1$Studio=="WB"),]
+
+ggplot(Data1, aes(x=Genre, y=Gross...US, size=Budget...mill.,color=Studio)) + 
+    geom_jitter()
